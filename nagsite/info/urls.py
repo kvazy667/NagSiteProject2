4014095ad56dc32b2eaf5dab1e25d16f5fd77d34
kvazy667 <dimashishkin123456@gmail.com>
2024-04-04 14:42:28 +0300

    Добавили ссылку

--- nagsite/info/urls.py
+++ nagsite/info/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = (
     path("", views.start),
+    path("forms", views.forms),
 )
--- nagsite/info/views.py
+++ nagsite/info/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import projectForm
 
 def start(request):
     #тут прописывается весь бекэнд
     return render(request, 'info/start.html')
-    return render(request, 'info/forms.html')
+
+def forms(request):
+    if request.method == 'POST':
+        data = projectForm(request.POST)
+        if data.is_valid():
+            data.save()
+            return redirect("/admin/info/project/")
+    form = projectForm()
+    print(form)
+    context = {
+        'form':form
+    }
+    return render(request, 'info/forms.html', context)
+
+
